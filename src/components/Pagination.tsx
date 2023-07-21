4d80537f1f22b540fcd012992dc4255318cfa379
Tim Moss <tmoss726@hotmail.com>
2023-07-21 10:47:06 -0400

    Hover state fix

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -7,8 +7,14 @@ const Pagination: React.FC<PaginationProps> = ({
   postsPerPage,
   onPageChange,
 }) => {
-  const totalPages = React.useMemo(() => Math.ceil(totalCount / postsPerPage), [totalCount, postsPerPage]);
-  const pages = React.useMemo(() => new Array(totalPages).fill(''), [totalPages]);
+  const totalPages = React.useMemo(
+    () => Math.ceil(totalCount / postsPerPage),
+    [totalCount, postsPerPage]
+  );
+  const pages = React.useMemo(
+    () => new Array(totalPages).fill(''),
+    [totalPages]
+  );
   const handlePageChange = (page: number) => {
     onPageChange(page);
   };
@@ -44,7 +50,7 @@ const Pagination: React.FC<PaginationProps> = ({
               key={i}
               className={`m-2 p-2 shadow-sm rounded ${
                 currentPage === i + 1
-                  ? 'text-gray-800 bg-lime-300 hover:text-white hover:bg-red-900'
+                  ? 'text-gray-800 bg-lime-300'
                   : 'text-white bg-red-900  hover:text-gray-800 hover:bg-lime-300'
               }`}>
               {i + 1}
